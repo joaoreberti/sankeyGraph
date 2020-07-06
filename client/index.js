376a9f2c7c2308594c7f44fcf075a3046783e161
reberti <jreberti@leads4sales.pt>
2020-07-07 00:09:08 +0100

    lets see

--- client/index.js
+++ client/index.js
@@ -5,47 +5,54 @@ let values = []
 console.log(window.location)
 fetch('http://joaoreberti.tech:3010/getValues')
 .then((result => result.json())
-).then(data => data.map(item => {
+).then(data => {
+  
+  
+  data.map(item => {
   labels.push(item.field)
   values.push(item.value)
   console.log('joao', labels);
   console.log('reberti',values)
 
-  var data = {
-    type: "sankey",
-    orientation: "h",
-    node: {
-      pad: 15,
-      thickness: 30,
-      line: {
-        color: "black",
-        width: 0.5
-      },
-     label: labels,
-     color: ["blue", "blue", "blue", "blue", "blue", "blue"]
+})}
+
+
+
+var data = {
+  type: "sankey",
+  orientation: "h",
+  node: {
+    pad: 15,
+    thickness: 30,
+    line: {
+      color: "black",
+      width: 0.5
     },
-  
-    link: {
-      source: [0, 0, 0, 0, 0, 0, 0, 0 ],
-      target: [1, 2, 3, 4, 5, 6, 7, 8 ],
-      value:  values
-    }
-  }
-  
-  var data = [data]
-  
-  var layout = {
-    title: "Basic Sankey",
-    font: {
-      size: 10
-    }
-  }
-  
+   label: labels,
+   color: ["blue", "blue", "blue", "blue", "blue", "blue"]
+  },
 
-  
+  link: {
+    source: [0, 0, 0, 0, 0, 0, 0, 0 ],
+    target: [1, 2, 3, 4, 5, 6, 7, 8 ],
+    value:  values
+  }
+}
 
-  Plotly.react('myDiv', data, layout)
-}))
+var data = [data]
+
+var layout = {
+  title: "Basic Sankey",
+  font: {
+    size: 10
+  }
+}
+
+
+
+
+Plotly.react('myDiv', data, layout)
+)
 
 
 
